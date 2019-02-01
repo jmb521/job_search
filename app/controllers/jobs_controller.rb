8be3bf7416297dfa55b88312d1768edcd8de0b85
Jennifer Pazos <jmp521@gmail.com>
2019-01-31 21:16:35 -0600

    updated jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,39 @@
 class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(job_params)
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.delete
+  end
+
+  private
+
+  def job_params
+    params.require(:jobs).permit(:company, :link, :job_title, :job_description, :user_id)
+  end
+
 end
